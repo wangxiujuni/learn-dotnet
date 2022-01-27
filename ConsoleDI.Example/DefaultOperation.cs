91e5a174c23983ba0fcb5008414a3559c14db7c2
Julen <wangxiujuni@gmail.com>
2022-01-27 17:20:09 +0800

    learn DI

--- ConsoleDI.Example/DefaultOperation.cs
+++ ConsoleDI.Example/DefaultOperation.cs
@@ -4,5 +4,6 @@ namespace ConsoleDI.Example;
 
 public class DefaultOperation : ITransientOperation, IScopedOperation, ISingletonOperation
 {
+    // 获取 GUID 后 4 位
     public string OperationId { get; } = NewGuid().ToString()[^4..];
 }
--- ConsoleDI.Example/Program.cs
+++ ConsoleDI.Example/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ConsoleDI.Example;
+
+// 起一个 host
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+        // 注册三个不同 lifttime 的服务，由于 DefaultOperation 可以实现三个接口
+        services.AddTransient<ITransientOperation, DefaultOperation>()
+            .AddScoped<IScopedOperation, DefaultOperation>()
+            .AddSingleton<ISingletonOperation, DefaultOperation>()
+            .AddTransient<OperationLogger>())
+    .Build();
+
+ExemplifyScoping(host.Services, "Scope 1");
+ExemplifyScoping(host.Services, "Scope 2");
+
+// 运行 host
+await host.RunAsync();
+
+static void ExemplifyScoping(IServiceProvider services, string scope)
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+
+    OperationLogger logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}-Call 1 .GetRequiredService<OperationLogger>()");
+
+    Console.WriteLine("...");
+
+    logger = provider.GetRequiredService<OperationLogger>();
+    logger.LogOperations($"{scope}-Call 2 .GetRequiredService<OperationLogger>()");
+
+    Console.WriteLine();
+}
